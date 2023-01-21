d1bd90490e921bea2721ca91b95c89aece92e8d8
CODE-BUDDY <111114081+BUDDY1609@users.noreply.github.com>
2023-01-21 17:00:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us ❣ @tgnvs')
 
 # Custom Chats
 SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', 'tgnvs')
-FILE_CHANNEL = int(environ.get('FILE_CHANNEL', '')
+FILE_CHANNEL = int(environ.get('FILE_CHANNEL', RQST_LOG_CHANNEL)
 FILE_CHANNEL_LINK = environ.get('FILE_CHANNEL_LINK', 'https://t.me/+hT_0ruEiIDxmNTVl')
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/nvsmovielink/21')
 
